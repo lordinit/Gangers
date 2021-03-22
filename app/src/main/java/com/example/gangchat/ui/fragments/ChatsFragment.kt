f5d2239e97172667a1aa49cc66e06f33e25ff72d
l1zzard2444@gmail.com <Umatur310717>
2021-03-22 18:07:49 +0300

    First

---
+++ app/src/main/java/com/example/gangchat/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.gangchat.ui.fragments
+
+import com.example.gangchat.R
+
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
