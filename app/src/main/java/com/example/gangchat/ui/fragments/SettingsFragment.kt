d7333b4d401181d5b359eec0c58e7ee2b90d5eab
l1zzard2444@gmail.com <Umatur310717>
2021-03-31 13:52:12 +0300

    Change_name_settings

--- app/src/main/java/com/example/gangchat/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/gangchat/ui/fragments/SettingsFragment.kt
@@ -8,6 +8,7 @@ import com.example.gangchat.R
 import com.example.gangchat.activites.RegisterActivity
 import com.example.gangchat.utilits.AUTH
 import com.example.gangchat.utilits.replaceActivity
+import com.example.gangchat.utilits.replaceFragment
 
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
@@ -28,6 +29,7 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
                 AUTH.signOut()
                 (activity as MainActivity).replaceActivity(RegisterActivity())
             }
+            R.id.settings_menu_change_name -> replaceFragment(ChangeNameFragment())
         }
         return true
     }
